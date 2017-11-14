727766bd95cae6c17799199a66cfa5b027cdf958
Charles Lima <jlima.charles@gmail.com>
2017-11-14 20:23:48 -0200

    validações de campos

--- src/POSUPClass/POSUPClass/POSUPClass/CadastroPage.xaml.cs
+++ src/POSUPClass/POSUPClass/POSUPClass/CadastroPage.xaml.cs
@@ -11,5 +11,35 @@ namespace POSUPClass
         {
             InitializeComponent();
         }
+
+        public void onCadastrarClicked(object sender, EventArgs args)
+        {
+            var nameValue = nome.Text;
+            if (String.IsNullOrEmpty(nameValue)) {
+                DisplayAlert("Erro", "O campo nome não pode ser nulo", "OK");
+                return;
+            }
+
+            var emailValue = email.Text;
+            if (String.IsNullOrEmpty(emailValue))
+            {
+                DisplayAlert("Erro", "O campo email não pode ser nulo", "OK");
+                return;
+            }
+
+            var senhaValue = senha.Text;
+            if (String.IsNullOrEmpty(senhaValue))
+            {
+                DisplayAlert("Erro", "O campo senha não pode ser nulo", "OK");
+                return;
+            }
+
+            DisplayAlert("", "Cadastro realizado com sucesso", "OK");
+            this.Navigation.PopAsync();
+        }
+
+        public void onBackClicked(object sender, EventArgs args){
+            this.Navigation.PopAsync();
+        }
     }
 }
