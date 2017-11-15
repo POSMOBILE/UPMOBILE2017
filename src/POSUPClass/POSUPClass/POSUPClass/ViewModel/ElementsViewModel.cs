ffc5f04b9eaab450bb939c4f34b35a1b6a2722d8
carlosleandro1 <carlos.leandro1980@gmail.com>
2017-11-15 20:48:56 -0200

    Carlos Leandro TelaSubCategoria

---
+++ src/POSUPClass/POSUPClass/POSUPClass/ViewModel/ElementsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSUPClass.ViewModel
+{
+    public class ElementsViewModel
+    {
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Image { get; set; }
+
+
+    }
+}
