a88c3936aad23795a4eb25c067362a1fc14a0c5a
carlosleandro1 <carlos.leandro1980@gmail.com>
2017-11-15 21:02:16 -0200

    Tela de SubCategorias
    Tela de SubCategorias da tela lista de desejos.

---
+++ src/POSUPClass/POSUPClass/POSUPClass/Data/Professor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSUPClass.Data
+{
+    public class Professor
+    {
+        int id;
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        string picture;
+        public string Picture
+        {
+            get { return this.picture; }
+            set { this.picture = value; }
+        }
+
+        string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        float alunos;
+        public float Alunos
+        {
+            get { return this.alunos; }
+            set { this.alunos = value; }
+        }
+
+        int cursos;
+        public int Cursos
+        {
+            get { return this.cursos; }
+            set { this.cursos = value; }
+        }
+
+        string classificacao;
+        public string Classificacao
+        {
+            get { return this.classificacao; }
+            set { this.classificacao = value; }
+        }
+
+        string description;
+        public string Description
+        {
+            get { return this.description; }
+            set { this.description = value; }
+        }
+    }
+}
---
+++ src/POSUPClass/POSUPClass/POSUPClass/Data/ProfessorPerfilCursos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSUPClass.Data
+{
+    public class ProfessorPerfilCursos
+    {
+        int id;
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        int idProf;
+        public int IdProf
+        {
+            get { return this.idProf; }
+            set { this.idProf = value; }
+        }
+
+        string picture;
+        public string Picture
+        {
+            get { return this.picture; }
+            set { this.picture = value; }
+        }
+
+        string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        int stars;
+        public int Stars
+        {
+            get { return this.stars; }
+            set { this.stars = value; }
+        }
+
+        int votes;
+        public int Votes
+        {
+            get { return this.votes; }
+            set { this.votes = value; }
+        }
+
+        double discount;
+        public double Discount
+        {
+            get { return this.discount; }
+            set { this.discount = value; }
+        }
+        
+        double price;
+        public double Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+    }
+}
