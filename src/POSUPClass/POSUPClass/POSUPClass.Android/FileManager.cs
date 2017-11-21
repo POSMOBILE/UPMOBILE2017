ab0145b42f6de6cce727e8016fec01e46cca4ad9
Raphael Vinícius Corrêa <bugs.blacktoolkit@gmail.com>
2017-11-21 08:39:27 -0200

    correcao 2

---
+++ src/POSUPClass/POSUPClass/POSUPClass.Android/FileManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using POSUPClass;
+using Xamarin.Forms;
+
+
+  
+[assembly: Xamarin.Forms.Dependency(typeof(POSUPClass.Droid.DroidURLDialog))]
+namespace POSUPClass.Droid
+{
+
+    public class DroidURLDialog : IURLDialog
+    {
+        private string txt = null;
+
+        public string getText()
+        {
+            return txt;
+        }
+
+        public void show()
+        {
+            EditText et = new EditText(Forms.Context);
+            AlertDialog.Builder ad = new AlertDialog.Builder(Forms.Context);
+            ad.SetTitle("URL do vídeo:");
+            ad.SetView(et); // <----
+
+            ad.SetPositiveButton("OK",(s,ev)=>
+            {
+                IUploadWebService uploadWebService = DependencyService.Get<IUploadWebService>();
+
+                int st = Xamarin.Forms.Application.Current.MainPage.Navigation.NavigationStack.Count - 1;
+                ((UploadFile)Xamarin.Forms.Application.Current.MainPage.Navigation.NavigationStack[st]).SetURL(et.Text);
+
+                uploadWebService.uploadFileAsync(new Uri(et.Text), "");
+            });
+            ad.Show();
+
+            //AlertDialog add = ad.Create();
+
+
+        }
+    }
+}
---
+++ src/POSUPClass/POSUPClass/POSUPClass.Android/MockFileInterfaces.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+[assembly: Xamarin.Forms.Dependency(typeof(POSUPClass.MockUploadWebService))]
+namespace POSUPClass
+{
+
+    
+
+
+    public class MockUploadWebService : IUploadWebService
+    {
+
+
+        private static List<Uri> fotos = new List<Uri>();
+
+        public MockUploadWebService()
+        {
+
+        }
+
+        public void deleteFile(Uri uri)
+        {
+           
+
+            fotos.Remove(uri);
+        }
+
+        public Uri[] listFiles()
+        {
+            return fotos.ToArray<Uri>();
+        }
+
+        public void uploadFileAsync(Uri uri, string file)
+        {
+            fotos.Add(uri);
+        }
+    }
+}
